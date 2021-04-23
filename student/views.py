c5570f6f93b2e0e2de3fc7a93025fde61ff288ed
pradeep kumar ghantasala <pradeepghantasala@digital-nirvana.com>
2021-04-23 10:01:27 +0530

    this is my first commit

---
+++ student/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def student(request):
+    return render(request, 'student.html', {'name' : 'pradeep'})
+
+def add(request):
+    val1 = int(request.POST['value1'])
+    val2 = int(request.POST['value2'])
+    res = val1+val2
+    
+    return render(request, 'result.html', {'result' : res})
